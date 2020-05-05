cad4e7dddb4833c88a9fe2b95ef8fc42e6458b2f
TaruGouldberg <63569915+TaruGouldberg@users.noreply.github.com>
2020-05-05 05:30:06 +0000

    Sixth and Seventh Project

---
+++ MySixthProject/App copy.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+
+  const csuLogo = {
+    uri: "https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/rams.png",
+
+  }
+
+
+  return (
+    <View style={styles.container}>
+      
+      <Image
+        style={styles.localCSULogo}
+        corce={required('./assets/CSU-Ram.png')}
+      />
+
+      <Image
+        style={styles.urlCSULogo}
+        source={{
+          uri: "https://raw.githugusercontent.com/AbdunabiRamadan/CIS340/master/images/logo.png"
+        }}
+      />
+
+      
+      <Image
+      style={styles.stretchLogo}
+      source={csuLogo} />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+
+  localCSULogo: {
+    width: 55,
+    height: 65
+  },
+
+  urlCSULogo: {
+    width: 50,
+    height: 50
+  },
+
+  stretchLogo: {
+    height: 200,
+    resizeMode: 'center'
+  }
+});
